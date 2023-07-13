e54066b10a27aa1f223ee37ed16e1f2dcf48598e
Starbeamrainbowlabs <sbrl@starbeamrainbowlabs.com>
2023-07-13 02:42:19 +0100

    update filenames

--- src/app.mjs
+++ src/app.mjs
@@ -4,8 +4,8 @@ import VisualisationManager from './lib/VisualisationManager.mjs';
 
 window.addEventListener("load", async (event) => {
 	const manager = new VisualisationManager(
-		"./data/tweets-all-new-20220117-UMAPPED_d3.txt.gz",
-		"./data/tweets-all-new-20220117-UMAPPED_d2.tsv.gz"
+		"./data/tweets-all-new-20220117-glove_twitter_27B_50d-UMAPPED_d3.txt.gz",
+		"./data/tweets-all-new-20220117-glove_twitter_27B_50d-UMAPPED_d2.tsv.gz"
 	);
 	await manager.init();
 });
