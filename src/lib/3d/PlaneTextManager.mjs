0621bb1acc6e1f8a26f86635c3b1ff8080410e20
Starbeamrainbowlabs <sbrl@starbeamrainbowlabs.com>
2023-07-25 01:06:43 +0100

    start working on auto text display, but it's not finished yet

--- src/lib/3d/PlaneTextManager.mjs
+++ src/lib/3d/PlaneTextManager.mjs
@@ -1,12 +1,26 @@
 "use strict";
 
 class PlaneTextManager {
-	constructor() {
+	constructor(scene) {
+		this.scene = scene;
+		
 		this.planes = new Map();
 	}
 	
 	create(pos) {
-		
+		const plane = new BABYLON.MeshBuilder.CreatePlane("plane-text", {
+			width: 8, height: 8
+		});
+		plane.position = pos;
+		const texture = new BABYLON.DynamicTexture("text", 256, this.scene);
+		texture.drawText("flooding", null, null, "12px sans-serif", "");
+		texture.hasAlpha = true;
+		plane.material = diffuse(scene, new BABYLON.Color4(64, 44, 38));
+		plane.material.diffuseTexture = texture;
+		this.planes.set(pos.toString(), {
+			plane,
+			texture
+		});
 	}
 	
 	delete(pos) {
