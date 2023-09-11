0a564a35ac374e198b45c978c6c1a4ee618fb8c2
Starbeamrainbowlabs <sbrl@starbeamrainbowlabs.com>
2023-09-12 00:15:38 +0100

    start preparing for multi-filepath support loong way to go before it's actually implemented tho need a UI and a few other bits and bobs first

--- src/app.mjs
+++ src/app.mjs
@@ -3,9 +3,9 @@
 import VisualisationManager from './lib/VisualisationManager.mjs';
 
 window.addEventListener("load", async (event) => {
-	const manager = new VisualisationManager(
+	const manager = new VisualisationManager([
 		"./data/tweets-all-new-20220117-glove_twitter_27B_50d-UMAPPED_d3.txt.gz",
 		"./data/tweets-all-new-20220117-glove_twitter_27B_50d-UMAPPED_d2.tsv.gz"
-	);
+	]);
 	await manager.init();
 });
--- src/lib/VisualisationManager.mjs
+++ src/lib/VisualisationManager.mjs
@@ -5,19 +5,19 @@ import fetch_umap from "./io/fetch_umap.mjs";
 import babylon_renderer from "./3d/babylon_renderer.mjs";
 
 class VisualisationManager {
-	constructor(filepath_umap_3d, filepath_umap_2d) {
-		this.filepath_umap_3d = filepath_umap_3d;
-		this.filepath_umap_2d = filepath_umap_2d;
+	constructor(filepaths_umap) {
+		// TODO: Implement UI to choose file to load
+		this.filepaths_umap = filepaths_umap;
 		
 		this.canvas = document.querySelector("#render-canvas");
 	}
 	
 	async init() {
-		this.data_3d = await fetch_umap(this.filepath_umap_3d);
-		this.data_2d = await fetch_umap(this.filepath_umap_2d);
+		this.data = await fetch_umap(this.filepaths_umap[0]);
+		// this.data = await fetch_umap(this.filepath_umap_2d);
 		
 		
-		console.log(this.data_3d);
+		console.log(this.data);
 		
 		await babylon_renderer(this);
 	}
