3f0c6f74d059df93b37ed3c93d0cbe8345fc8f4a
Starbeamrainbowlabs <sbrl@starbeamrainbowlabs.com>
2024-01-06 20:38:41 +0000

    fetch_umap: force strings to be strings rather than autoconverted to floats

--- src/lib/io/fetch_umap.mjs
+++ src/lib/io/fetch_umap.mjs
@@ -23,13 +23,15 @@ async function fetch_umap(source_url) {
 	
 	TSV.header = false;
 	let result = TSV.parse(response_str).map(item => [
-		item[0],
+		`${item[0]}`, // Force string since npm's tsv doesn't support disabling parseFloat to let us do it manually
 		...item.slice(1).map(parseFloat)
 	]);
 	if(DO_FILTER) {
 		const count_before_filter = result.length;
 		// result = result.filter(row => !word_detector.test(row[0]));
-		result = result.filter(row => !word_detector.check(row[0]));
+		result = result.filter(row => {
+			return !word_detector.check(row[0])
+		});
 		const count_after_filter = result.length;
 		const count_filtered = count_before_filter - count_after_filter;
 		console.log(`[fetch_umap:FILTER] Filter active | ${count_before_filter} → ${count_after_filter}; filtered out ${count_filtered} rude words (${((count_filtered/count_before_filter)*100).toFixed(2)}%)`);
