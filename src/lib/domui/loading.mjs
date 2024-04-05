b6bc84b713a1ff9593fc8b420caee0b3fd427744
Starbeamrainbowlabs <sbrl@starbeamrainbowlabs.com>
2024-04-05 18:25:29 +0100

    make loading class actually do something

--- src/lib/domui/loading.mjs
+++ src/lib/domui/loading.mjs
@@ -1,11 +1,15 @@
 "use strict";
 
+var loading_time = null;
+
 function load_start() {
+	loading_time = new Date();
 	document.body.classList.add(`loading`);
 }
 
 function load_end() {
 	document.body.classList.remove(`loading`);
+	console.log(`Loading complete in ${(new Date() - loading_time)}s`)
 }
 
 export default {
